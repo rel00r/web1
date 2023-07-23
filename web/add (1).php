05064ab4ee7172866854ffcd7f8331ae192b6365
Rimas Ali <136834166+rel00r@users.noreply.github.com>
2023-07-24 00:52:15 +0300

    Add files via upload

--- web/add (1).php
+++ web/add (1).php
@@ -20,20 +20,8 @@
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
-    $sql = "SELECT buttons FROM dir ORDER BY id DESC LIMIT 1";
-
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            session_start();
-            $_SESSION["lastInsertedValue"] = $row["buttons"];
-        }
-    } else {
-        echo "No results";
-    }
-
+   
     $conn->close();
 
-    header("Location: index.html");
+    header("Location: index.html ");
 ?>
--- web/in.php
+++ web/in.php
@@ -1,6 +1,45 @@
 <?php
-    // Start the session
-    session_start();
+// Replace these with your actual database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "directions";
+
+// Create a connection to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Define the table name and column representing the order of entries (e.g., timestamp or an auto-incrementing ID)
+$tableName = "dir";
+$orderColumn = "buttons"; // Replace this with the actual column name
+
+// Query to select the last row from the table
+$sql = "SELECT buttons FROM dir ORDER BY id DESC LIMIT 1";
+
+// Execute the query
+$result = $conn->query($sql);
+
+// Check if the query was successful
+if ($result && $result->num_rows > 0) {
+    // Fetch the last row as an associative array
+    $lastRow = $result->fetch_assoc();
+
+    // Now you can access the data from the last row
+    // For example, if you have a column named 'name', you can access it like this:
+    $lastvalue = $lastRow['buttons'];
+
+    // Display or use the data as needed
+   
+} else {
+    echo "No data found in the table.";
+}
+
+// Close the database connection
+$conn->close();
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +51,6 @@
 
 <body>
     <!-- Display the last inserted value -->
-    <p>Last inserted value: <?php echo $_SESSION["lastInsertedValue"]; ?></p>
+    <p> <?php  print $lastvalue ; ?></p>
 </body>
 </html>
